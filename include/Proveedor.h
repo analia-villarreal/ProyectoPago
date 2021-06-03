9f4a4c08a24d7777d5aab01dfea48000795aa01c
Analia <ana.lau.villarreal@gmail.com>
2021-06-03 19:24:57 -0300

    Primer comitt

---
+++ include/Proveedor.h
@@ -0,0 +1,34 @@
+#ifndef PROVEEDOR_H
+#define PROVEEDOR_H
+
+
+class Proveedor
+{
+
+    private:
+        int _idProveedor;
+        char _razonSocial[45];
+        int _CUIT;
+        int _juridiccion;
+        Impuesto _retIIBB;
+        Impuesto _retGanancias;
+
+    public:
+        Proveedor();
+        void setId(int);
+        void setRazonSocial(char*);
+        void setCUIT();
+        void setJuridiccion();
+        int getId();
+        char * getRazonSocial();
+        int getCUIT();
+        int getJuridiccion();
+        void cargar();
+        void mostrar();
+        bool guardarEnDisco();
+        void guardarEnDisco(int);
+        bool leerEnDisco();
+
+};
+
+#endif // PROVEEDOR_H
