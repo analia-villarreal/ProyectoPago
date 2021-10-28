33d4a1471c5da5eea99060f14dd9c0dea390c2be
Analia <ana.lau.villarreal@gmail.com>
2021-10-28 00:06:26 -0300

    todos los sets gets de op

--- src/OrdenDePago.cpp
+++ src/OrdenDePago.cpp
@@ -53,25 +53,111 @@ void OrdenDePago::setFormaDePago(MediosDePagos formaPago) {
 
     _formDePago=formaPago;
 }
-bool OrdenDePago::setEstadoOP(int estado) {
+void OrdenDePago::setEstadoOP(bool estado) {
+
+    _estadoOP=estado;
+}
+int OrdenDePago::getIdOP() {
+
+    return _idOP;
+}
+Fecha OrdenDePago::getFechaContabilizacion() {
+
+    return _fechaContabilizacion;
+}
+int OrdenDePago::getTipoOP() {
+
+    return _tipoOP;
+}
+int OrdenDePago::getIdProveedor() {
+
+    return _idProveedor;
+}
+Proveedor OrdenDePago::getRazonSocial(int pos) {
+
+    return _razonSocial;
+}
+int OrdenDePago::getNumComp() {
+
+    return _numComp;
+}
+float OrdenDePago::getRetIIBB() {
+
+    return _retIIBB;
+}
+float OrdenDePago::getRetGanancias() {
+
+    return _retGanancias;
+}
+float OrdenDePago::getImporteAPagar() {
+
+    return _importeAPagar;
+}
+float OrdenDePago::getImporteTotal() {
+
+    return _importeTotal;
+}
+int OrdenDePago::getEstado() {
+
+    return _estadoOP;
+}
+void OrdenDePago::cargar(int tipo) {
+
 
 
 }
-int OrdenDePago::getIdOP() {}
-Fecha OrdenDePago::getFechaContabilizacion() {}
-int OrdenDePago::getTipoOP() {}
-int OrdenDePago::getIdProveedor() {}
-Proveedor OrdenDePago::getRazonSocial(int) {}
-int OrdenDePago::getNumComp() {}
-float OrdenDePago::getRetIIBB() {}
-float OrdenDePago::getRetGanancias() {}
-float OrdenDePago::getImporteAPagar() {}
-float OrdenDePago::getImporteTotal() {}
-int OrdenDePago::getEstado() {}
-void OrdenDePago::cargar(int) {}
-void OrdenDePago::mostrar() {}
-bool OrdenDePago::leerDeDisco(int) {}
-bool OrdenDePago::guardarEnDisco() {}
-void OrdenDePago::guardarEnDisco(int) {}
-void OrdenDePago::listarOrdenesDePago() {}
-void OrdenDePago::listarOrdenadoFechaConta() {}
+void OrdenDePago::mostrar() {
+
+
+
+
+}
+bool OrdenDePago::leerDeDisco(int pos) {
+
+    bool lectura;
+    FILE *p;
+    p = fopen("ordenesdepago.dat", "rb");
+    if (p == NULL)
+    {
+        return false;
+    }
+    fseek(p, sizeof(OrdenDePago)*pos, SEEK_SET);
+    lectura = fread(this, sizeof(OrdenDePago), 1, p);
+    fclose(p);
+    return lectura;
+}
+bool OrdenDePago::guardarEnDisco() {
+
+    bool guardo;
+    FILE *p;
+    p = fopen("ordenesdepago.dat", "ab");
+    if (p == NULL)
+    {
+        return false;
+    }
+    guardo = fwrite(this, sizeof(OrdenDePago), 1, p);
+    fclose(p);
+    return guardo;
+
+}
+void OrdenDePago::guardarEnDisco(int pos) {
+    bool guardo;
+    FILE *p;
+    p = fopen("comprobantes.dat", "rb+");
+    if (p == NULL){
+        return;
+    }
+    fseek(p, sizeof(Comprobante)*pos, SEEK_SET);
+    guardo = fwrite(this, sizeof(Comprobante), 1, p);
+    fclose(p);
+    return;;
+
+}
+void OrdenDePago::listarOrdenesDePago() {
+
+
+}
+void OrdenDePago::listarOrdenadoFechaConta() {
+
+
+}
